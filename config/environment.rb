de53eeb7b6121bfbe922807e58d174cba72582b3
chrisbodhi <cboette@gmail.com>
2014-02-24 08:46:04 -0600

    Ready Mailer for production.

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 ToDoApp::Application.initialize!
 
-ActionMailer::Base.delivery_method = :letter_opener
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
+# ActionMailer::Base.delivery_method = :letter_opener
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.raise_delivery_errors = true
